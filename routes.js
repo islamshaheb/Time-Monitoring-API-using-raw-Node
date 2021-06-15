22ed1f75541897c7eab289414f62f0af54e4b58f
Mojahidul Islam <mujahidulislam9345@gmail.com>
2021-06-15 19:49:01 +0600

    Token

--- routes.js
+++ routes.js
@@ -11,10 +11,12 @@
 // dependencies
 const { sampleHandler } = require('./handlers/routeHandlers/sampleHandler');
 const { userHandler } = require('./handlers/routeHandlers/userHandler');
+const { tokenHandler } = require('./handlers/routeHandlers/tokenHandler.js');
 
 const route = {
   sample: sampleHandler,
   user: userHandler,
+  token: tokenHandler,
 };
 
 module.exports = route;
